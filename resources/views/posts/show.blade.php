56204819525e9dcb2e396e3da697b015eae5a308
Dai16 <daiyouao1016@icloud.com>
2022-02-14 16:38:56 +0900

    記事詳細画面

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,7 +14,9 @@
         <h1>Blog name</h1>
    
             <div class="post">
-               <h2 class="title">{{ $post->title }}</h2>
+               <h2 class="title">{{ $post->title }}</h2> 
+
+               
                <p class="body">{{ $post->body }}</p>
                <p class="updated_at">{{ $post->updated_at }}</p>
              </div>
